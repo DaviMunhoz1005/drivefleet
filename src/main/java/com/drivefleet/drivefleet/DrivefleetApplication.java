43dc09058593f38f0538060c22f7b72149eb1e65
Davi Munhoz <davimunhoz1005@gmail.com>
2025-11-24 20:31:07 -0300

    feat(setup): create setup for the project

---
+++ src/main/java/com/drivefleet/drivefleet/DrivefleetApplication.java
@@ -0,0 +1,13 @@
+package com.drivefleet.drivefleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivefleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrivefleetApplication.class, args);
+	}
+
+}
