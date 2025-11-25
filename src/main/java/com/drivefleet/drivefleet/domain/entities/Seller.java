fdb426a4359504e46e024ade997f2e2a621176cf
Davi Munhoz <davimunhoz1005@gmail.com>
2025-11-24 21:57:45 -0300

    feat(seller-entity): creates seller entity with user relationship

---
+++ src/main/java/com/drivefleet/drivefleet/domain/entities/Seller.java
@@ -0,0 +1,28 @@
+package com.drivefleet.drivefleet.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_SELLER")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "CHAR(36)")
+    private UUID id;
+
+    @OneToOne(optional = false)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private Long registrationNumber;
+}
