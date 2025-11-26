69545f607d3a4e1f613fed6d2b379e0d32a30d6e
Davi Munhoz <davimunhoz1005@gmail.com>
2025-11-25 22:09:13 -0300

    feat(saler-order-entity): create sales order entity with order status and relationship with other entities

---
+++ src/main/java/com/drivefleet/drivefleet/domain/entities/SalesOrder.java
@@ -0,0 +1,57 @@
+package com.drivefleet.drivefleet.domain.entities;
+
+import com.drivefleet.drivefleet.domain.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_SALES_ORDER")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SalesOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(columnDefinition = "CHAR(36)")
+    private UUID id;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+    private LocalDate conclusionDate;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalValue;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+
+    @OneToOne
+    @JoinColumn(name = "vehicle_id", unique = true)
+    private Vehicle vehicle;
+
+    @PrePersist
+    public void prePersist() {
+        this.creationDate = LocalDate.now();
+        if (this.status == null) {
+            this.status = OrderStatus.PENDING;
+        }
+    }
+}
+
