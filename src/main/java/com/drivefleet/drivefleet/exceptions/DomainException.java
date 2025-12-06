2e6a2167ef136c158c4b5f17745bd9e99ba95b73
Davi Munhoz <davimunhoz1005@gmail.com>
2025-12-06 14:04:29 -0300

    feat(domain-exception): create domain exception with basic infos

---
+++ src/main/java/com/drivefleet/drivefleet/exceptions/DomainException.java
@@ -0,0 +1,23 @@
+package com.drivefleet.drivefleet.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public abstract class DomainException extends RuntimeException {
+
+    private final String code;
+    private final HttpStatus status;
+
+    protected DomainException(String code, String message, HttpStatus status) {
+        super(message);
+        this.code = code;
+        this.status = status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+}
